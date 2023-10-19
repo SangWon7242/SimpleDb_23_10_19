1e2696b63b106289972dbcd21afee815813321fb
SangWon7242 <gotkdqja@gmail.com>
2023-10-19 23:49:17 +0900

    run 기능 구현

--- src/main/java/com/sbs/simpleDb/SimpleDb.java
+++ src/main/java/com/sbs/simpleDb/SimpleDb.java
@@ -1,9 +1,33 @@
 package com.sbs.simpleDb;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SimpleDb {
+
+  private final Connection connection;
   public SimpleDb(String host, String username, String password, String dbName) {
+    int port = 3306;
+    String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName
+        + "?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull";
+    try {
+      connection = DriverManager.getConnection(url, username, password);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public void setDevMode(boolean mode) {
+  }
+
+  public void run(String sql) {
+    try {
+      Statement statement = connection.createStatement();
+      statement.executeUpdate(sql);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
