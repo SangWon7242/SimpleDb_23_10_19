5a78c2f5837bcca55a744792bdad225a90fae8bd
SangWon7242 <gotkdqja@gmail.com>
2023-10-19 23:04:41 +0900

    세팅

---
+++ src/main/java/com/sbs/simpleDb/SimpleDb.java
@@ -0,0 +1,9 @@
+package com.sbs.simpleDb;
+
+public class SimpleDb {
+  public SimpleDb(String host, String username, String password, String dbName) {
+  }
+
+  public void setDevMode(boolean mode) {
+  }
+}
